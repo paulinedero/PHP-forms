1b5ca813efcd7fb4d4dccc7bb0ece783c4b768bc
Pauline De Ro <pauline@bobex.net>
2021-11-09 08:43:18 +0100

    exercice done / add front validation

--- forms/form.php
+++ forms/form.php
@@ -2,83 +2,53 @@
 <html>
 
 <head>
-    <style>
-        .error {
-            color: #FF0000;
-        }
-    </style>
 </head>
 
 <body>
     <?php
-        $lastnameErr = $firstnameErr = $emailErr = $telErr = $subjectErr = $messageErr = "";
         $lastname = $firstname = $email = $phone = $subject = $message = "";
-
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (empty($_POST["user_lastname"])) {
-                $lastnameErr = "Lastname is required";
-            }
-
-            if (empty($_POST["user_firstname"])) {
-                $firstnameErr = "Firstname is required";
-            }
-            
-            if (empty($_POST["user_email"])) {
-                $emailErr = "Email is required";
-            } else {
-                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    $emailErr = "Invalid email format";
-                }
-            }
-
-            if (empty($_POST["user_phone"])) {
-                $telErr = "Phone is required";
-            }
-
-            if (empty($_POST["user_subject"])) {
-                $subjectErr = "Subject is required";
-            }
-
-            if (empty($_POST["user_message"])) {
-                $messageErr = "Message is required";
-            }
     ?>
 
     <form action="thanks.php" method="post">
         <div>
             <label for="nom">Nom :</label>
-            <input type="text" id="nom" name="user_lastname" value=<?php echo $lastname?>>
-            <br>
+            <input type="text" id="nom" name="user_lastname" required value=<?php echo $lastname?>>
+            <br><br>
         </div>
         <div>
             <label for="prenom">Prénom :</label>
-            <input type="text" id="prenom" name="user_firstname" value=<?php echo $firstname?>>
-            <br>
+            <input type="text" id="prenom" name="user_firstname" required value=<?php echo $firstname?>>
+            <br><br>
         </div>
         <div>
             <label for="courriel">Courriel :</label>
-            <input type="email" id="courriel" name="user_email" value=<?php echo $email?>>
+            <input type="email" id="courriel" name="user_email" required value=<?php echo $email?>>
+            <br><br>
         </div>
         <div>
             <label for="phone">Téléphone :</label>
-            <input type="tel" id="phone" name="user_phone" value=<?php echo $phone?>>
+            <input type="tel" id="phone" name="user_phone" required value=<?php echo $phone?>>
+            <br><br>
         </div>
         <div>
             <label for="subject">Sujet :</label>
-            <select id="subject" name="user_subject">
+            <select id="subject" name="user_subject" required>
                 <option <?php if (isset($subject) && $subject=="info") echo "checked";?> value="info">Demande d'informations</option>
                 <option <?php if (isset($subject) && $subject=="complaint") echo "checked";?> value="complaint">Plainte</option>
                 <option <?php if (isset($subject) && $subject=="remark") echo "checked";?> value="remark">Remarque</option>
                 <option <?php if (isset($subject) && $subject=="other") echo "checked";?> value="other">Autre</option>
             </select>
+            <br><br>
         </div>
         <div>
             <label for="message">Message :</label>
-            <textarea id="message" name="user_message" value=<?php echo $message?>></textarea>
+            <textarea id="message" name="user_message" required value=<?php echo $message?>></textarea>
+            <br><br>
         </div>
         <div class="button">
             <button type="submit">Envoyer votre message</button>
         </div>
     </form>
+
 </body>
 </html>
--- forms/thanks.php
+++ forms/thanks.php
@@ -1,8 +1,68 @@
-<p>
-Merci <?php echo $_POST['user_firstname']?> de nous avoir contacté à propos de “<?php echo $_POST['user_subject']?>”.<br>
-Un de nos conseiller vous contactera soit à l’adresse <?php echo $_POST['user_email']?> ou par téléphone au <?php echo $_POST['user_phone']?> dans les plus brefs délais pour traiter votre demande :<br> 
-<?php echo $_POST['user_message']?>
-</p>
+<!DOCTYPE HTML>
+<html>
 
+<head>
+    <style>
+        .error {
+            color: #FF0000;
+        }
+    </style>
+</head>
 
+<body>
+    <?php
+        $email = $_POST["user_email"];
+        $lastnameErr = $firstnameErr = $emailErr = $telErr = $subjectErr = $messageErr = "";
 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($_POST["user_lastname"])) {
+                $lastnameErr = "Lastname is required";
+            }
+
+            if (empty($_POST["user_firstname"])) {
+                $firstnameErr = "Firstname is required";
+            }
+            
+            if (empty($_POST["user_email"])) {
+                $emailErr = "Email is required";
+            } else {
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $emailErr = "Invalid email format";
+                }
+            }
+
+            if (empty($_POST["user_phone"])) {
+                $telErr = "Phone is required";
+            }
+
+            if (empty($_POST["user_subject"])) {
+                $subjectErr = "Subject is required";
+            }
+
+            if (empty($_POST["user_message"])) {
+                $messageErr = "Message is required";
+            }
+        }
+    ?>
+    <?php
+        if ($lastnameErr || $firstnameErr || $emailErr || $telErr ||  $subjectErr || $messageErr ) {
+    ?>
+            <div class="error"> <?php echo $lastnameErr; ?></div>
+            <div class="error"> <?php echo $firstnameErr; ?></div>
+            <div class="error"> <?php echo $emailErr; ?></div>
+            <div class="error"> <?php echo $telErr; ?></div>
+            <div class="error"> <?php echo $subjectErr; ?></div>
+            <div class="error"> <?php echo $messageErr; ?></div>
+    <?php        
+        } else {
+    ?>
+            <p>
+                Merci <?php echo $_POST['user_firstname'] ?> de nous avoir contacté à propos de “<?php echo $_POST['user_subject'] ?>”.<br>
+                Un de nos conseiller vous contactera soit à l’adresse <?php echo $_POST['user_email'] ?> ou par téléphone au <?php echo $_POST['user_phone'] ?> dans les plus brefs délais pour traiter votre demande :<br>
+                <?php echo $_POST['user_message'] ?>
+            </p>
+    <?php
+        }
+    ?>
+</body>
+</html>
